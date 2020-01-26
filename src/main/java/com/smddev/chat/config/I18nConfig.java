12d036b25342b96bc33d69a2a97bb0e3c0efcfeb
nikita-butko <nikita.og@gmail.com>
2020-01-26 13:25:44 +0300

    I18n is added (but only for English)

---
+++ src/main/java/com/smddev/chat/config/I18nConfig.java
@@ -0,0 +1,31 @@
+package com.smddev.chat.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class I18nConfig implements WebMvcConfigurer {
+
+    @Bean
+    public static MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("en"));
+        return localeResolver;
+    }
+
+}
+
